4b66afa5f41b8d3b41fba28b61d16491bef683ed
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-02-26 16:11:31 -0500

    Adding stored procedures lesson script

---
+++ StoredProcedures/NorthwindStoredProcedures.sql
@@ -0,0 +1,60 @@
+﻿USE Northwind;
+GO
+
+-- no param
+CREATE PROCEDURE sp_ProductsToReorder
+AS
+SELECT ProductName, UnitsInStock
+FROM Products
+WHERE UnitsInStock < 5
+ORDER BY UnitsInStock DESC, ProductName
+GO
+
+EXEC sp_ProductsToReorder
+
+-- params
+CREATE PROCEDURE sp_ProductsToReorder2(
+    @UnitsInStock INT
+)
+AS
+SELECT ProductName, UnitsInStock
+FROM Products
+WHERE UnitsInStock < @UnitsInStock
+ORDER BY UnitsInStock DESC, ProductName
+GO
+
+EXEC sp_ProductsToReorder2 4
+
+-- alter an sp
+ALTER PROCEDURE sp_ProductsToReorder
+AS
+SELECT ProductName, UnitsInStock
+FROM Products
+WHERE UnitsInStock < 10
+ORDER BY UnitsInStock DESC, ProductName
+GO
+
+EXEC sp_ProductsToReorder
+
+-- drops
+-- exists...drop...create pattern is sometimes preferred over using alters
+IF EXISTS(
+    SELECT *
+    FROM INFORMATION_SCHEMA.ROUTINES
+    WHERE ROUTINE_NAME = 'ProductsToReorder2'
+    )
+BEGIN
+    DROP PROCEDURE ProductsToReorder2
+END
+GO
+
+CREATE PROCEDURE ProductsToReorder2(
+    @UnitsInStock int
+)
+AS
+
+SELECT ProductName, UnitsInStock
+FROM Products
+WHERE UnitsInStock < @UnitsInStock
+ORDER BY UnitsInStock DESC, ProductName
+GO
