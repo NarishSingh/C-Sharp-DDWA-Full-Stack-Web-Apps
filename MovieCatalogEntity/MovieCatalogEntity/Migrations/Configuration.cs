f0f67cbc861aa12896417d0ca6c6da02d6331e05
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-03-04 15:59:48 -0500

    Enabled and added migrations

---
+++ MovieCatalogEntity/MovieCatalogEntity/Migrations/Configuration.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace MovieCatalogEntity.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<MovieCatalogEntity.Models.EF.MovieCatalogEntities>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+    } 
+}
