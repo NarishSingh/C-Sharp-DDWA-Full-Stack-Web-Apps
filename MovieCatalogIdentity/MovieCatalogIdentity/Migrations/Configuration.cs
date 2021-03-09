af47c29803b35fb319b0db5b7f59a364580a43f6
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-03-09 16:15:37 -0500

    Migrations and seed data for user account

---
+++ MovieCatalogIdentity/MovieCatalogIdentity/Migrations/Configuration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MovieCatalogIdentity.Models.Identity;
+
+namespace MovieCatalogIdentity.Migrations
+{
+    internal sealed class
+        Configuration : DbMigrationsConfiguration<MovieCatalogIdentity.Models.Identity.MovieCatalogDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MovieCatalogDbContext context)
+        {
+            //load user and role managers w custom models
+            var userMgr = new UserManager<AppUser>(new UserStore<AppUser>(context));
+            var roleMgr = new RoleManager<AppRole>(new RoleStore<AppRole>(context));
+
+            //check if roles loaded, if not, create admin
+            if (roleMgr.RoleExists("admin"))
+            {
+                return;
+            }
+
+            roleMgr.Create(new AppRole
+                {
+                    Name = "admin"
+                }
+            );
+
+            //default user
+            var user = new AppUser
+            {
+                UserName = "admin"
+            };
+
+            //create with manager class, add to admin role
+            userMgr.Create(user, "testing123");
+            userMgr.AddToRole(user.Id, "admin");
+        }
+    }
+}
