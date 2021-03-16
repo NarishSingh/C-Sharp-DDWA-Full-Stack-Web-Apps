83659b6d09c0f40b279f9ae75070b6eebd7852c2
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-03-16 14:45:44 -0400

    Adding File upload lesson proj

---
+++ FileUploads/FileUploads/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using FileUploads.Models;
+
+namespace FileUploads.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            FileUploadVM model = new FileUploadVM();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(FileUploadVM model)
+        {
+            //if a file was uploaded
+            if (model.Upload != null && model.Upload.ContentLength>0)
+            {
+                //convert web path to a dir path and save locally
+                string path = Path.Combine(Server.MapPath("~/Uploads"), Path.GetFileName(model.Upload.FileName));
+                
+                model.Upload.SaveAs(path);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+        
+        /*Get binary rep of a file for db upload*/
+        private byte[] ConvertPostedFileToByteArray(HttpPostedFileBase file)
+        {
+            byte[] imageByte = null;
+            BinaryReader rdr = new BinaryReader(file.InputStream);
+            imageByte = rdr.ReadBytes((int) file.ContentLength);
+            return imageByte;
+        }
+    }
+}
---
+++ FileUploads/FileUploads/Models/FileUploadVM.cs
@@ -0,0 +1,9 @@
+﻿using System.Web;
+
+namespace FileUploads.Models
+{
+    public class FileUploadVM
+    {
+        public HttpPostedFileBase Upload { get; set; }
+    }
+}
