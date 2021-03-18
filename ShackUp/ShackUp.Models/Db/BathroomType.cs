9957683146d9cfdc8da170d5cf093fb3435e7db3
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-03-17 20:23:05 -0400

    Starting Models layer

---
+++ ShackUp/ShackUp.Models/Db/BathroomType.cs
@@ -0,0 +1,8 @@
+﻿namespace ShackUp.Models.Db
+{
+    public class BathroomType
+    {
+        public int BathroomTypeId { get; set; }
+        public string BathroomTypeName { get; set; }
+    }
+}
---
+++ ShackUp/ShackUp.Models/Db/Contact.cs
@@ -0,0 +1,8 @@
+﻿namespace ShackUp.Models.Db
+{
+    public class Contact
+    {
+        public int ListingId { get; set; }
+        public string UserId { get; set; }
+    }
+}
