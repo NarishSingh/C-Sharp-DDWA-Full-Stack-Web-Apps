ac3296ccc6c77eb726abf07b2f0aff90e2b39996
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-03-23 20:56:21 -0400

    Adding Visual Studio boilerplates for ASP Identity

---
+++ ShackUp/ShackUp.UI/Utilities/AuthorizeUtilities.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Web.Mvc;
+using ShackUp.UI.Models.Identity;
+
+namespace ShackUp.UI.Utilities
+{
+    public class AuthorizeUtilities
+    {
+        public static string GetUserId(Controller controller)
+        {
+            var userMgr = new UserManager<AppUser>(new UserStore<AppUser>(new ShackUpDbContext()));
+            var user = userMgr.FindByName(controller.User.Identity.Name);
+            return user.Id;
+        }
+    }
+}
