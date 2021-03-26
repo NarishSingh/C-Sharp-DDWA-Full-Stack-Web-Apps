03640feec51607d84fb7acd193845d4d0b3721cd
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-03-26 17:45:36 -0400

    Working on listing details views and controller actions

--- ShackUp/ShackUp.UI/Controllers/ListingsController.cs
+++ ShackUp/ShackUp.UI/Controllers/ListingsController.cs
@@ -1,9 +1,25 @@
 ﻿using System.Web.Mvc;
+using ShackUp.Data.Factories;
+using ShackUp.Data.Interfaces;
+using ShackUp.Models.Queried;
+using ShackUp.UI.Utilities;
 
 namespace ShackUp.UI.Controllers
 {
     public class ListingsController : Controller
     {
-        
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            if (Request.IsAuthenticated)
+            {
+                ViewBag.UserId = AuthorizeUtilities.GetUserId(this);
+            }
+
+            IListingRepo repo = ListingRepositoryFactory.GetRepository();
+            ListingItem model = repo.ReadDetailedListingById(id);
+
+            return View(model);
+        }
     }
 }
