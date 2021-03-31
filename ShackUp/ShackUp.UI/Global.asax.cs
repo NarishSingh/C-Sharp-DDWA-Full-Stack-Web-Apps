03e7afe2ec3b0c7b5f6022b2391d90809c824c8e
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-03-31 15:44:49 -0400

    fixed search functionality

--- ShackUp/ShackUp.UI/Global.asax.cs
+++ ShackUp/ShackUp.UI/Global.asax.cs
@@ -14,10 +14,10 @@ namespace ShackUp.UI
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register); //needs to register before the RouteConfig
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
