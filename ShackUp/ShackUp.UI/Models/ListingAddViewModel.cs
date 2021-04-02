a3163c90f6fb7d6f2dd9a3f60afc4c5c2840c1f4
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-04-02 16:02:46 -0400

    Server side validation

--- ShackUp/ShackUp.UI/Models/ListingAddViewModel.cs
+++ ShackUp/ShackUp.UI/Models/ListingAddViewModel.cs
@@ -1,17 +1,67 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ShackUp.Models.Db;
 
 namespace ShackUp.UI.Models
 {
-    public class ListingAddViewModel
+    public class ListingAddViewModel : IValidatableObject
     {
         public IEnumerable<SelectListItem> States { get; set; }
         public IEnumerable<SelectListItem> BathroomTypes { get; set; }
         public Listing Listing { get; set; }
         public HttpPostedFileBase ImageUpload { get; set; }
-        
-        
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+
+            if (string.IsNullOrEmpty(Listing.Nickname))
+            {
+                errors.Add(new ValidationResult("City is required"));
+            }
+
+            if (string.IsNullOrEmpty(Listing.City))
+            {
+                errors.Add(new ValidationResult("City is required"));
+            }
+
+            if (string.IsNullOrEmpty(Listing.ListingDescription))
+            {
+                errors.Add(new ValidationResult("Description is required"));
+            }
+
+            if (ImageUpload != null && ImageUpload.ContentLength > 0)
+            {
+                //even if uploaded, verify if file extension is valid for persistence 
+                string[] extensions = {".jpg", ".png", ".gif", ".jpeg"};
+
+                string uploadExtension = Path.GetExtension(ImageUpload.FileName);
+
+                if (!extensions.Contains(uploadExtension))
+                {
+                    errors.Add(new ValidationResult("Image file must be jpg, png, gif, or jpeg"));
+                }
+            }
+            else
+            {
+                errors.Add(new ValidationResult("Image required"));
+            }
+
+            if (Listing.Rate <= 0)
+            {
+                errors.Add(new ValidationResult("Rate must be > $0"));
+            }
+
+            if (Listing.SquareFootage <= 0)
+            {
+                errors.Add(new ValidationResult("Square footage must be < 0 sq. ft."));
+            }
+
+            return errors;
+        }
     }
 }
