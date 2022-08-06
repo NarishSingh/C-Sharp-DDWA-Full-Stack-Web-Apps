ac6182288f03a35bc6809145610b3d7ce6ab5cf2
NarishSingh <narish.singh786@gmail.com>
2022-08-06 16:04:52 -0400

    Fixes, debugging errors

--- DiceSim/DiceSim/Program.fs
+++ DiceSim/DiceSim/Program.fs
@@ -3,11 +3,11 @@
 printfn "===DICE SIM==="
 printfn "1 | Stats"
 printfn "2 | Lucky 7's"
-
 printf "Choose module to load: "
+
 let input: string = Console.ReadLine()
 match Int32.TryParse input with
-| false, _ -> printfn $"%s{input} is not a valid selection..."
+| false, _ -> failwith $"%s{input} is not a valid selection..."
 | true, num ->
     match num with
     | 1 -> // Stats
@@ -22,20 +22,20 @@ match Int32.TryParse input with
         | true, numDice ->
             // validate num of rolls
             match Int32.TryParse rollsInput with
-            | false, _ -> failwith "Invalid number of rolls..."
+            | false, _ -> failwith "Could not parse number of rolls..."
             | true, numRolls ->
                 match numRolls > 0 with
                 | false -> failwith "Invalid number of rolls..."
                 | true ->  
                     let rng: Random = Random()
-                    let rollDice (diceCt: int) = rng.Next(diceCt, (diceCt * 6) + 1)
+                    let rollDice (diceCt: int): int = rng.Next(diceCt, (diceCt * 6) + 1)
                     
                     let rolls: int list = [for i in 1 .. numRolls -> rollDice numDice] // list comprehension
-                    match rolls with
-                    | _  -> rolls
-                        |> List.average
-                        |> printfn $"Average of rolls = %s"
-                    | [] -> 0
+                    match List.isEmpty rolls with
+                    | true -> printfn "List empty, average = 0"
+                    | false -> rolls
+                        |> List.average rolls
+                        |> printfn "Average of rolls = %s"
     | 2 -> // Lucky 7's
         ()
-    | _ -> printfn "Not a valid module to run..."
+    | _ -> failwith "Not a valid module to run..."
